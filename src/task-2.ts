5246fbd063cdbb707ebf40e8444efacb79a5fb56
Maksim_Regeda <maxymregeda669@gmail.com>
2026-01-11 14:17:36 +0200

    fix 8 6

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,10 +1,10 @@
-interface Tablet {
+interface Product {
     readonly id: number;
     title: string;
     description?: string;
 }
 
-const product: Tablet = {
+const product: Product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
